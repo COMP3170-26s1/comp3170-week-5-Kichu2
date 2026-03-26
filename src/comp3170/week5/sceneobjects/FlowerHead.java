3009c89adf153fa686acce863bd59c5bb993ade4
Felix Han <kiato8369@gmail.com>
2026-03-26 12:43:05 +1100

    asdad

--- src/comp3170/week5/sceneobjects/FlowerHead.java
+++ src/comp3170/week5/sceneobjects/FlowerHead.java
@@ -18,7 +18,6 @@ public class FlowerHead extends SceneObject {
 
 	private Vector4f[] vertices;
 	private int vertexBuffer;
-	Private int indexBuffer;
 
 	public FlowerHead(int nPetals, Vector3f colour) {
 		
@@ -28,37 +27,6 @@ public class FlowerHead extends SceneObject {
 		
 		shader = ShaderLibrary.instance.compileShader(VERTEX_SHADER, FRAGMENT_SHADER);		
 		petalColour = colour;
-		
-		float rInner = 0.1f;
-		float rOuter = 0.4f;
-		vertices = new Vector4f[nPetals * 4];
-		int[] indices = new int[nPetals * 6];
-		for (int i = 0; i < nPetals; i++) {
-			float angle1= i * 2.0f * (float)Math.PI / nPetals;
-			float angle2= (i + 1) * 2.0f * (float)Math.PI / nPetals;
-			
-			Vector4f inner1 = new Vector4f((float)Math.cos(angle1)* rInner, (float)Math.sin(angle1) * rInner, 0, 1);
-			Vector4f outer1 = new Vector4f((float)Math.cos(angle1)* rOuter, (float)Math.sin(angle1) * rOuter, 0, 1);
-			Vector4f inner2 = new Vector4f((float)Math.cos(angle1)* rInner, (float)Math.sin(angle2) * rInner, 0, 1);
-			Vector4f outer2 = new Vector4f((float)Math.cos(angle1)* rOuter, (float)Math.sin(angle2) * rOuter, 0, 1);
-			
-			int vIndex = i*4;
-			vertices[vIndex] = inner1;
-			vertices[vIndex+1] = outer1;
-			vertices[vIndex+2] = inner2;
-			vertices[vIndex+3] = outer2;
-			
-			int iIndex = i*6;
-			indices[iIndex] = vIndex;
-			indices[iIndex+1] = vIndex+1;
-			indices[iIndex+2] = vIndex+2;
-			indices[iIndex+3] = vIndex+3;
-			indices[iIndex+4] = vIndex+4;
-			indices[iIndex+5] = vIndex+5;
-			
-		}
-		vertexBuffer = GLBuffers.createBuffer(vertices);
-		indexBuffer = GLBuffers.createIndexBuffer(indices);
 	}
 
 	public void update(float dt) {
@@ -67,11 +35,5 @@ public class FlowerHead extends SceneObject {
 
 	public void drawSelf(Matrix4f mvpMatrix) {
 		// TODO: Add any appropriate draw code. (TASK 1)
-		shader.enable();
-		shader.setUniform("u_mvpMatrix", mvpMatrix);
-		shader.setAttribute("a_position", vertexBuffer);
-		shader.setUniform("u_colour", petalColour);
-		
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
 	}
 }
